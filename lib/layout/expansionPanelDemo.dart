b88f39f7bc9502fc916013ae21c832b8439c869d
Rishika Garg <gargrishika5@gmail.com>
2021-05-05 18:25:01 +0530

    expansionPanelDemo.dart

--- lib/layout/expansionPanelDemo.dart
+++ lib/layout/expansionPanelDemo.dart
@@ -9,6 +9,38 @@ class ExpansionPanelDemo extends StatefulWidget {
 
 class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
 
+  List<Item> _data = generateItems(10);
+
+  Widget _buildPanelDemo(){
+    return ExpansionPanel(
+      expansionCallback: (int index, bool isExpanded){
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item){
+        return ExpansionPanel{
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text(item.headerValue),
+            );
+          },
+          body: ListTile(
+               title: Text(item.expandedValue),
+               subtitle: Text('To delete this panel, tap the trash icon'),
+               trailing: Icon(Icon.delete),
+               ontap: () {
+                 setState(() {
+                   _data.removeWhere((currentItem) => item == currentItem);
+        });
+               },
+        ),
+        isExpanded: item,isExpanded
+        };
+      }).toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -16,8 +48,28 @@ class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
         appBar: AppBar(
           title: Text('Expansion Panel Demo'),
         ),
-        body: Center(
-          child: Text('Show expansion panel demo here'),
-      ),);
+        body: SingleChildScrollView(
+          child: Container(
+            child: _builderPanelDemo(),
+          ),
+        ),
+    );
   }
+}
+
+class Item{
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+
+  Item({this.expandedValue, this.headerValue, this.isExpanded = false});
+}
+
+List<Item> generateItems(int numberOfItems){
+  return List.generate(numberOfItems, (index) {
+    return Item(
+     headerValue: 'Panel $index',
+     expandedValue: 'This is item number $index',
+    );
+  });
 }
